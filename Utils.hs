37096fb7b3478b5c641400cef47a85299a01f847
James Larisch <jamesfordummies@gmail.com>
2015-12-10 19:34:04 -0500

    this works. i fear the unreadable JSON

--- Utils.hs
+++ Utils.hs
@@ -11,7 +11,7 @@ import Data.Hashable
 majority :: Int
 majority = 2 -- because 1 is always implied (1 + 2)
 
-sendCooldown = 0.05
+sendCooldown = 0.04
 
 cooledOff :: UTCTime -> UTCTime -> Bool
 cooledOff now t = diff > sendCooldown
@@ -62,7 +62,7 @@ getNextCommands [] _ = []
 getNextCommands slog nextIndex
   | nextIndex > length slog = error $ "getNextCommands: nextIndex greater than slength " ++ (show nextIndex) ++ " : " ++ (show $ length slog)
   | nextIndex == length slog = []
-  | otherwise = take 17 $ drop nextIndex slog
+  | otherwise = drop nextIndex slog
 
 upToDate :: [Command] -> Int -> Int -> Bool
 upToDate [] _ _ = True
